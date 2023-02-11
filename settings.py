e4345ea1d2d505acf3e1eb185c2469dbea7567cb
dominika-tasting <k.or121@mail.ru>
2023-02-11 13:41:25 +0400

    24.7.2

---
+++ settings.py
@@ -0,0 +1,6 @@
+valid_email = 'k.or121@mail.ru'
+valid_password = 'ajlpvhxvhklbgf'
+empty_email = ''
+empty_password = ''
+empty_auth_key = {'key': ''}
+incorrect_auth_key = {'key': '111111111111111111111111111111111111111111111'}
